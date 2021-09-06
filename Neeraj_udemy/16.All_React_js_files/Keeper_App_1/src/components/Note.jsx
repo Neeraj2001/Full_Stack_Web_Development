03ff0f1e451c318063ac1ab2ea65072e9a11a348
Neeraj <42690154+Neeraj2001@users.noreply.github.com>
2021-09-06 13:55:47 +0530

    Add files via upload

---
+++ Neeraj_udemy/16.All_React_js_files/Keeper_App_1/src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className = "note" >
+        <h1>Assignment </h1>
+        <p>Work Assignment . Means a position or post calling for specified duties to which an employee is assigned for a definite or indefinite period of time but which has not been designated as a work classification. Duty assignment is interchangeable with work assignment.</p>
+    </div>
+}
+
+export default Note;
