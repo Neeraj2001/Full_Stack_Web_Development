53a89097bfd8b966ef34c57a13d8b2b43e389310
Neeraj <42690154+Neeraj2001@users.noreply.github.com>
2021-09-07 12:12:29 +0530

    Add files via upload

---
+++ Neeraj_udemy/16.All_React_js_files/Emojipedia/src/emojipedia.js
@@ -0,0 +1,32 @@
+const Emojipedia = [
+  {
+    id: 1,
+    emoji: "😎 ",
+    name: " Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach"
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  }
+];
+
+export default Emojipedia;
