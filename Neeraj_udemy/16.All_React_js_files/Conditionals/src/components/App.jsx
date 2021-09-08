3b1963d661cc978e20327a55acab155e112dffb6
Neeraj <42690154+Neeraj2001@users.noreply.github.com>
2021-09-08 16:14:43 +0530

    convert functions to arrow

--- Neeraj_udemy/16.All_React_js_files/Conditionals/src/components/App.jsx
+++ Neeraj_udemy/16.All_React_js_files/Conditionals/src/components/App.jsx
@@ -3,19 +3,12 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  function increase() {
-    setCount(count + 1);
-  }
-
-  function decrease() {
-    setCount(count - 1);
-  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
-      <button onClick={decrease}>-</button>
-      <button onClick={increase}>+</button>
+      <button onClick={() => setCount(count-1)}>-</button>
+      <button onClick={() => setCount(count+1)}>+</button>
     </div>
   );
 }
